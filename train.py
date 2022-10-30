efeb13893c04a707a28aca1a7cfe7b4184569509
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-30 22:42:07 +0800

    the name is Violet Evergarden.

--- train.py
+++ train.py
@@ -17,20 +17,25 @@ train_parser.add_argument("--batch_size",    default = 4)
 train_parser.add_argument("--visualize_itr", default = 3)
 train_config = train_parser.parse_args(args = [])
 
-print(train_parser)
+for item in train_config._get_kwargs():
+    print("-",item[0],"is",item[1])
 
 def train_render_field(model,train_config = train_config):
-    optimizer = torch.optim.Adam(model.parameters(), lr = 2e-4)
-    if train_config.dataset == "sprite3":
-        dataset = Sprite3("train")
+    optimizer = torch.optim.Adam(model.parameters(), lr = 2e-4) # initialize the optimizer
+
+    # set up the experiment dataset
+    if train_config.dataset == "sprite3":dataset = Sprite3("train")
+    if train_config.dataset == "clevr4": dataset = Clevr4("train")
+
+    # inialize the dataloader according to the train config
     dataloader = DataLoader(dataset, batch_size = train_config.batch_size,shuffle = True)
     for epoch in range(train_config.epoch):
-        total_loss = 0
+        total_loss = 0 # log the total_loss
         itr = 0
         for sample in dataloader:
             im = sample["image"];itr += 1
 
-            recon = im
+            recon = im # calculate the model output
 
             # visualize the training image and output if in vis-iter
             if itr % train_parser.visualize_itr == 0:
@@ -46,3 +51,9 @@ def train_render_field(model,train_config = train_config):
             total_loss += working_loss # add the working_loss to the total loss
         
         print("epoch:{} loss:{}".format(epoch,total_loss.detach()))
+
+    print("training of neural render field completed.")
+
+if __name__ == "__main__":
+    # and let the training begin.
+    pass
